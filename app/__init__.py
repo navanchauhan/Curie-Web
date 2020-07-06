5339f4096d9b8d9a81b90d7997dc99a814b37f22
Navan Chauhan <navanchauhan@gmail.com>
2020-07-06 18:55:30 +0530

    added MariaDB config

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,11 @@ from flask import Flask
 # Config Values
 # location where file uploads will be stored
 UPLOAD_FOLDER = './app/static/uploads'
+DB_HOST = '192.168.1.6' #'navanspi.duckdns.org'
+DB_PORT = 3306
+DB_USER = 'curieweb'
+DB_PASSWORD = 'curie-web-russian-54'
+DB_NAME = 'curie'
 # needed for session security, the flash() method in this case stores the message
 # in a session
 SECRET_KEY = 'Sup3r$3cretkey'
