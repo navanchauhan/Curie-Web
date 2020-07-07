6a67fd7e8b7e3244fb009e7101e5c501bf2439d8
Navan Chauhan <navanchauhan@gmail.com>
2020-07-07 12:22:30 +0530

    removed extra files and changed zi to zipArchive

--- app/dock_docker.py
+++ app/dock_docker.py
@@ -8,7 +8,7 @@ mycursor.execute(sql_select_Query)
 
 records = mycursor.fetchall()
 
-def email(zi):
+def email(zipArchive):
     import smtplib 
     from email.mime.multipart import MIMEMultipart 
     from email.mime.text import MIMEText 
@@ -26,7 +26,7 @@ def email(zi):
     
     msg.attach(MIMEText(body, 'plain')) 
     filename = "Curie_Web_Results_Job_ID_" + str(jobID) + ".zip"
-    attachment = open((str(zi) + ".zip"), "rb") 
+    attachment = open((str(zipArchive) + ".zip"), "rb") 
     p = MIMEBase('application', 'octet-stream') 
     p.set_payload((attachment).read()) 
     encoders.encode_base64(p) 
